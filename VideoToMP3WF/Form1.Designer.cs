3a7b27fcb034e814abdd8496389dcc74799e1aed
Matthew Stroble <chimiara999@msn.com>
2023-01-22 21:57:27 -0700

    Initial Downloads

--- VideoToMP3WF/Form1.Designer.cs
+++ VideoToMP3WF/Form1.Designer.cs
@@ -28,12 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.youtubeLink = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Youtube URL";
+            // 
+            // youtubeLink
+            // 
+            this.youtubeLink.Location = new System.Drawing.Point(111, 23);
+            this.youtubeLink.Name = "youtubeLink";
+            this.youtubeLink.Size = new System.Drawing.Size(628, 23);
+            this.youtubeLink.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Download";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.Location = new System.Drawing.Point(157, 70);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(38, 15);
+            this.StatusLabel.TabIndex = 3;
+            this.StatusLabel.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.youtubeLink);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox youtubeLink;
+        private Button button1;
+        private Label StatusLabel;
     }
 }
--- VideoToMP3WF/Form1.cs
+++ VideoToMP3WF/Form1.cs
@@ -1,3 +1,8 @@
+using MediaToolkit.Model;
+using MediaToolkit;
+using Microsoft.Win32;
+using VideoLibrary;
+
 namespace VideoToMP3WF
 {
     public partial class Form1 : Form
@@ -5,6 +10,41 @@ namespace VideoToMP3WF
         public Form1()
         {
             InitializeComponent();
+        }
+
+        // using Microsoft.Win32;
+        string GetDownloadFolderPath()
+        {
+            return Registry.GetValue(@"HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders", "{374DE290-123F-4565-9164-39C4925E467B}", String.Empty).ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var downloadPath = GetDownloadFolderPath();
+            var youtubeURL = youtubeLink.Text.Trim();
+            StatusLabel.Text= "Downloading";
+            SaveMP3(downloadPath, youtubeURL);
+            StatusLabel.Text = "Download Complete";
+        }
+
+        private async Task SaveMP3(string SaveToFolder, string VideoURL)
+        {
+            string source = SaveToFolder;
+            var youtube = YouTube.Default;
+            var vid = youtube.GetVideo(VideoURL);
+            string videopath = Path.Combine(source, vid.FullName);
+            File.WriteAllBytes(videopath, vid.GetBytes());
+            string MP3Name = vid.FullName.ToString();
+
+            var inputFile = new MediaFile { Filename = Path.Combine(source, vid.FullName) };
+            var outputFile = new MediaFile { Filename = Path.Combine(source, $"{MP3Name}.mp3") };
+
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+                engine.Convert(inputFile, outputFile);
+            }
+            File.Delete(Path.Combine(source, vid.FullName));
         }
     }
 }
